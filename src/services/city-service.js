13aef0110ff2d032860f0184cfbc4a77fef3fe2e
Abhilash <jampaniabhilash81@gmail.com>
2023-10-01 13:12:42 +0530

    added our city service

---
+++ src/services/city-service.js
@@ -0,0 +1,48 @@
+const {cityRepository}=require('../repository/index');
+
+class CityService{
+    constructor(){
+        this.cityRepository=new cityRepository();
+    }
+    async createCity(data){
+        try {
+            const city=await this.cityRepository.createCity(data);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong");
+            return {error};
+        }
+    }
+
+    async deleteCity(city_id){
+        try {
+            const city=await this.cityRepository.deleteCity(city_id);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong");
+            return {error};
+        }
+    }
+
+    async updateCity(city_id,data){
+        try {
+            const city=await this.cityRepository.updateCity(city_id,data);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong");
+            return {error};
+        }
+    }
+
+    async getCity(city_id){
+        try {
+            const city=await this.cityRepository.getCity(city_id);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong");
+            return {error};
+        }
+    }
+}
+
+module.exports=CityService;
