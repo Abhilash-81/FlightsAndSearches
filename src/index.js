af5c6ccbdb91a8f6e6533863476c27e675fa51c0
Abhilash <jampaniabhilash81@gmail.com>
2023-10-09 12:34:03 +0530

    Done with routes for city

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-const express=require('express');
-const bodyParser=require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const {PORT}=require('./config/serverConfig');
+const { PORT } = require("./config/serverConfig");
+const ApiRoutes = require("./routes/index");
 
-
-const setupAndStartServer=async()=>{
-    const app=express();
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:true}));
-    app.listen(PORT,async ()=>{
-        console.log(`Server Started at ${PORT}`);
-    })
-}
+const setupAndStartServer = async () => {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use("/api", ApiRoutes);
+  app.listen(PORT, async () => {
+    console.log(`Server Started at ${PORT}`);
+  });
+};
 
 setupAndStartServer();
