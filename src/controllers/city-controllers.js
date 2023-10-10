51cb6761cc425a99dbb9201cdb4f340013849f16
Abhilash <jampaniabhilash81@gmail.com>
2023-10-10 19:10:58 +0530

    Added Routed to Cities

--- src/controllers/city-controllers.js
+++ src/controllers/city-controllers.js
@@ -1,6 +1,7 @@
 const { CityService } = require("../services/index");
 
 const cityService = new CityService();
+
 const create = async (req, res) => {
   try {
     const city = await cityService.createCity(req.body);
@@ -63,9 +64,9 @@ const get = async (req, res) => {
 
 const update = async (req, res) => {
   try {
-    const city = await cityService.updateCity(req.params.id, req.data);
+    const response = await cityService.updateCity(req.params.id, req.body);
     return res.status(200).json({
-      data: city,
+      data: response,
       success: true,
       message: "successfully updated a city",
       err: {},
